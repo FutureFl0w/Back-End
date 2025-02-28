737861c1c864441902cd8707e73b40e9b02b5122
javier.cader <javier.cader@ct.uneatlantico.es>
2025-02-28 10:49:44 +0100

    fix(template): added template for backend service

---
+++ jest.setup.ts
@@ -0,0 +1,5 @@
+process.env.JWT_SECRET = 'dummy';
+process.env.GOOGLE_CLIENT_ID = 'dummy';
+process.env.GOOGLE_CLIENT_SECRET = 'dummy';
+process.env.GOOGLE_REDIRECT_URI = 'dummy';
+process.env.NODE_ENV = 'test';
---
+++ src/config/logger.config.ts
@@ -0,0 +1,39 @@
+import { createLogger, format, transports } from 'winston';
+import { config } from './environment.config';
+
+const { combine, timestamp, printf, colorize, errors } = format;
+
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+  return `${timestamp} [${level}]: ${stack || message}`;
+});
+
+export const logger = createLogger({
+  level: config.env === 'beta' ? 'debug' : 'info',
+  format: combine(
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    errors({ stack: true }),
+    colorize({ all: config.env === 'beta' }),
+    logFormat,
+  ),
+  transports: [
+    new transports.Console(),
+    ...(config.env === 'production'
+      ? [
+          new transports.File({ filename: 'logs/error.log', level: 'error' }),
+          new transports.File({ filename: 'logs/combined.log' }),
+        ]
+      : []),
+  ],
+  exitOnError: false,
+});
+
+logger.add(
+  new transports.Console({
+    level: 'http',
+    format: combine(
+      colorize({ all: config.env === 'development' }),
+      timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      logFormat,
+    ),
+  }),
+);
